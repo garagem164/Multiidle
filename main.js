e5056045cce5b39668485420a5f98a43fd8ecffe
Garagem1:64 <garagem1.64miniaturas@gmail.com>
2026-09-17 14:45:06 -0300

    Update and rename manifest.json to main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    title: "Poke Multi Manager - 4 Contas",
+    autoHideMenuBar: true,
+    webPreferences: {
+      webviewTag: true,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
